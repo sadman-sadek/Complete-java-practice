3d1e688a595e12cd0c95edc06b4628c3ea8c5311
sadman-sadek <tasinsadmansadek@gmail.com>
2026-08-27 19:51:29 +0600

    java day 2

--- final_variables_05.java
+++ final_variables_05.java
@@ -2,8 +2,11 @@ public class final_variables_05 {
    public static void main(String[]args){
    final int X;
    X =23;
-   int X =12;
-   /*here the program is wrong because we 
+   /*
+   if i assign a value of x
+   again this will show something is wrong cause we already
+   asign a fixed value of x. 
+   here the program is wrong because we 
    already assign a fixed value of X!*/
    System.out.println(X);
   
---
+++ taking_input_08.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class taking_input_08 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.println("Your name is: " + name);
+        System.out.println("Your age is: " + age);
+
+        input.close();
+    }
+}
