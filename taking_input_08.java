079e3ff58bdb13db97e58693fc025446c7340d22
sadman-sadek <tasinsadmansadek@gmail.com>
2026-08-29 20:17:41 +0600

    day 3 of java

--- taking_input_08.java
+++ taking_input_08.java
@@ -1,19 +1,9 @@
 import java.util.Scanner;
-
-public class taking_input_08 {
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = input.nextLine();
-
-        System.out.print("Enter your age: ");
-        int age = input.nextInt();
-
-        System.out.println("Your name is: " + name);
-        System.out.println("Your age is: " + age);
-
-        input.close();
+public class taking_input_08{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter your name : ");
+        String name=sc.nextLine();
+        System.out.print("your name = "+name);
     }
 }
