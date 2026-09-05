5746150b0720d27b7e472202120d7d8e25724ed0
sadman <sadmansadek0@gmail.com>
2026-09-05 13:48:57 +0600

    learned taking input from user

--- taking_input_08.java
+++ taking_input_08.java
@@ -1,9 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class taking_input_08{
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter your name : ");
-        String name=sc.nextLine();
-        System.out.print("your name = "+name);
+    public static void main(String []arg){
+        System.out.println("Enter your name, batch and roll no: ");
+        Scanner info = new Scanner(System.in);
+
+
+        System.out.print("Enter your name: ");  
+        String name = info.nextLine();
+        
+        
+
+        System.out.print("Enter your Roll no : ");
+        int rollNo = info.nextInt();
+        
+
+
+        System.out.print("Enter your Batch: "); 
+        int batch = info.nextInt();
+        
+
+        System.out.println("Your Name: " + name + " ");
+        System.out.println("Your Roll No: " + rollNo + " " );
+        System.out.println("Batch no : " + batch + " " );
+
+        if ( batch >45){
+            System.out.println("senior");
+        }
+        else {
+            System.out.println(" junior!");
+        }   
+        }
     }
-}
