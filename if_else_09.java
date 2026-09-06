dc248b7ebda22dc8cf160a6d504ef85c94e33efa
sadman <sadmansadek0@gmail.com>
2026-09-06 20:59:15 +0600

    if else

---
+++ if_else_09.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class if_else_09{
+    public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("enter your age :");
+    int age = input.nextInt();
+    
+
+    System.out.println("your age is :" + age );
+    if(age>=18){
+        System.out.println("so,you are an adult!Congrats !😍");
+    }
+    
+else {
+    System.out.println("sorry! You are not an adult😒");
+}
+  
+    if (age % 2 == 0) {
+        System.out.println("You age is in even numbers !👌");
+    }
+   else {
+     System.out.print(" your age is  in odd numbers!😁");
+   }
+    
+
+
+    }
+}
